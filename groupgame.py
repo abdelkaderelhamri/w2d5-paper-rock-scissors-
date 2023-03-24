b6939d7d5793ffe8221723249037b8771aecb63c
Daniel Wilson <wilsonsdaniels@gmail.com>
2023-03-24 12:32:01 -0400

    first commit

---
+++ groupgame.py
@@ -0,0 +1,34 @@
+import random
+
+computer_choices =["rock", "paper", "scissors"]
+
+while True:
+    player_choice = input("Enter your choice(rock, paper, scissors), or 'Q' to end the game: ")
+
+    if player_choice == 'q':
+        print("Thanks for playing")
+        break
+
+    if player_choice not in computer_choices:
+        print("invalid choice. please type rock, paper, or scissor.")
+        continue
+
+    computer_choice = random.choice(computer_choices)
+    print (computer_choice)
+
+    if player_choice == computer_choice:
+        print("game tied")
+    elif player_choice == "rock" and computer_choice == "paper":
+        print("you lose")
+    elif player_choice == "rock" and computer_choice == "scissors":
+        print("you win")
+    elif player_choice == "paper" and computer_choice == "scissors":
+        print("you lose")
+    elif player_choice == "paper" and computer_choice == "rock":
+        print("you win")
+    elif player_choice == "scissors" and computer_choice == "rock":
+        print("you lose")
+    elif player_choice == "scissors" and computer_choice == "paper":
+        print("you win")
+        
+    
